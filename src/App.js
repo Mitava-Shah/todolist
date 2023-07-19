bfda7563f1f353bdc705a5a9157fa476ed8b9247
mitava-shah <mitvahshah792@gmail.com>
2023-07-19 23:07:09 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
+import React,{useState} from "react"
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const[cities,setcities]=useState([])
+  const[cities1,setcities1]=useState("")
+function show(){
+  setcities([...cities,cities1])
+  setcities1("")
 }
 
+  return (
+    <>
+    <div className="container">
+      <input type="text" value={cities1} onChange={(e)=>setcities1(e.target.value)} placeholder="add city" />
+  
+    <button onClick={show}>Add</button>
+    </div>
+      {cities.map((item,index)=> (<ul>
+        <li key="index">{item}</li>
+      </ul>
+      ))}
+     
+    </>
+  );
+
+}
 export default App;
